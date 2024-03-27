9858a49d4ea51fbbfbd26e10b19d0da7132cc310
eceykilic <eceykilic@gmail.com>
2024-03-27 17:40:24 +0300

    solutions

---
+++ src/main/java/org/example/arge/GasPoweredCar.java
@@ -0,0 +1,39 @@
+package org.example.arge;
+
+public class GasPoweredCar extends CarSkeleton{
+    private double avgKmPerLiter;
+    private int cylinders;
+
+    public GasPoweredCar(String name, String description, double avgKmPerLiter, int cylinders) {
+        super(name, description);
+        this.avgKmPerLiter = avgKmPerLiter;
+        this.cylinders = cylinders;
+    }
+
+    @Override
+    public String startEngine() {
+        runEngine(this);
+        return getName() + " car starting...";
+    }
+
+    @Override
+    public String drive() {
+        return "run from gaspowered car";
+    }
+
+    public double getAverageKmPerLiter() {
+        return avgKmPerLiter;
+    }
+
+    public void setAvgKmPerLiter(double avgKmPerLiter) {
+        this.avgKmPerLiter = avgKmPerLiter;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public void setCylinders(int cylinders) {
+        this.cylinders = cylinders;
+    }
+}
